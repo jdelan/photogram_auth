055e2c056f2ef174bbc12008ef69f44964731986
Jesse Delan <jesse.delan@users.noreply.github.com>
2017-05-24 18:18:45 -0500

    added auth & valids, continue #13

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524225855) do
+ActiveRecord::Schema.define(version: 20170524230425) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "photo_id"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id"
