51462e1a4fa8a2677efe895edeaef238d6112ad0
Jesse Delan <jesse.delan@users.noreply.github.com>
2017-05-26 11:20:22 -0500

    100%! Yayyy

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
 belongs_to :photo
 belongs_to :user
 
-validates :username, :presence => true
+validates :user_id, :presence => true
 validates :photo_id, :presence => true
 validates :body, :presence => true
 end
